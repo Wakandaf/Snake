c9a0bf798013885f64446bf235fe2353b261427a
jpcode <83139455+Wakandaf@users.noreply.github.com>
2021-07-23 17:54:24 -0400

    Update fruit.js

--- fruit.js
+++ fruit.js
@@ -4,9 +4,9 @@ function Fruit() {
 
     this.pickLocation = function() {
         this.x = (Math.floor(Math.random() *
-            columns - 1) + 2) * scale;
+            columns - 1) + 1) * scale;
         this.y = (Math.floor(Math.random() *
-            rows - 1) + 2) * scale;
+            rows - 1) + 1) * scale;
     }
 
     this.draw = function() {
